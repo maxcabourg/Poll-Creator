8e6e89f8ae72c7cf2c479c0053056d059b2b4de2
Max Cabourg <maxcabourg@hotmail.fr>
2016-04-28 15:49:50 +0200

    Creation of class linking to database
    In this class, the constructor will read a file config not show in
    github and will open a connection to the database with these
    informations.

---
+++ src/database/Database.java
@@ -0,0 +1,53 @@
+package database;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Database {
+	
+	private static final String FICHIER_PROPERTIES       = "/database/database.properties";
+    private static final String PROPERTY_URL             = "url";
+    private static final String PROPERTY_DRIVER          = "driver";
+    private static final String PROPERTY_USERNAME 		 = "nomutilisateur";
+    private static final String PROPERTY_PASSWORD   	 = "motdepasse";
+    
+    private Connection conn;
+    
+    public Database()
+    {
+    	Properties properties = new Properties();
+		String url;
+		String driver;
+		String nomUtilisateur;
+		String motDePasse;
+
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream fichierProperties = classLoader.getResourceAsStream( FICHIER_PROPERTIES );
+		
+		try {
+			properties.load( fichierProperties );
+			url = properties.getProperty( PROPERTY_URL );
+	        driver = properties.getProperty( PROPERTY_DRIVER );
+	        nomUtilisateur = properties.getProperty( PROPERTY_USERNAME);
+	        motDePasse = properties.getProperty(PROPERTY_PASSWORD);
+	        Connection conn = DriverManager.getConnection(url, nomUtilisateur, motDePasse);
+		} catch (IOException e) {
+			System.out.println("Erreur : fichier de configuration introuvable.");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Erreur lors de la connexion à la base de données.");
+			e.printStackTrace();
+		}
+		        
+    }
+    
+    public Connection getConnexion()
+    {
+    	return conn;
+    }
+
+}
