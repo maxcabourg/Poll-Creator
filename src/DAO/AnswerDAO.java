9be071f150d8121e22587f56db95dc686fd58bc8
max.cabourg <max.cabourg@etu.umontpellier.fr>
2016-04-29 12:43:51 +0200

    Full implementation of AnswerDAO
    The AnswerDAO CRUD pattern is now fully implemented.

--- src/DAO/AnswerDAO.java
+++ src/DAO/AnswerDAO.java
@@ -1,40 +1,63 @@
 package DAO;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import POJO.Answer;
+import POJO.Poll;
 
 public class AnswerDAO extends DAO<Answer> {
 
 	@Override
-	public boolean create(Answer obj) throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean create(Answer answer) throws SQLException {
+		return true;
 	}
 
 	@Override
-	public boolean delete(Answer obj) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean delete(Answer answer) throws SQLException {
+		stmt = connect.prepareStatement("DELETE FROM Answer WHERE id_answer = ?");
+		stmt.setInt(1,  answer.getId());
+		stmt.executeUpdate();
+		stmt.close();
+		return true;
 	}
 
 	@Override
-	public boolean update(Answer obj) throws SQLException {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean update(Answer answer) throws SQLException {
+		stmt = connect.prepareStatement("UPDATE Answer SET content = ? WHERE id_answer = ?");
+		stmt.setString(1, answer.getContent());
+		stmt.setInt(2, answer.getId());
+		stmt.executeUpdate();
+		stmt.close();
+		return true;
 	}
 
 	@Override
 	public Answer find(int id) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		stmt = connect.prepareStatement("SELECT * FROM Answer WHERE id_answer = ?");
+		stmt.setInt(1, id);
+		ResultSet rs = stmt.executeQuery();
+		int answer_id = rs.getInt("id_answer");
+		String content = rs.getString("content");
+		rs.close();
+		stmt.close();
+		return new Answer(answer_id, content);
 	}
 
 	@Override
 	public ArrayList<Answer> getAll() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Answer> listAnswers = new ArrayList<Answer>();
+		stmt = connect.prepareStatement("SELECT COUNT(*) as count FROM Answer");
+		ResultSet rs = stmt.executeQuery();
+		int count = rs.getInt("count");
+		for(int i = 1; i<=count; i++){
+			listAnswers.add(find(i));
+		}
+		rs.close();
+		stmt.close();
+		return listAnswers;
 	}
 
 }
