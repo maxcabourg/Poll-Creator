fcbf2688c1dced68afcb051564af4badf60f723c
Max Cabourg <maxcabourg@hotmail.fr>
2016-04-30 14:00:56 +0200

    Implementation : UserDAO & User POJO

---
+++ src/DAO/UserDAO.java
@@ -0,0 +1,72 @@
+package DAO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import POJO.Answer;
+import POJO.User;
+
+public class UserDAO extends DAO<User> {
+
+	@Override
+	public boolean create(User user) throws SQLException {
+		stmt = connect.prepareStatement("INSERT INTO User (pseudo, password, mail) VALUES (?, ?, ?)");
+		stmt.setString(1, user.getPseudo());
+		stmt.setString(2, user.getPassword());
+		stmt.setString(2, user.getEmail());
+		stmt.executeUpdate();
+		stmt.close();
+		return true;
+	}
+
+	@Override
+	public boolean delete(User user) throws SQLException {
+		stmt = connect.prepareStatement("DELETE FROM User WHERE id_user = "+user.getId());
+		stmt.executeUpdate();
+		stmt.close();
+		return true;
+	}
+
+	@Override
+	public boolean update(User user) throws SQLException {
+		// TODO Auto-generated method stub
+		stmt = connect.prepareStatement("UPDATE User SET pseudo = ?, password = ?, mail = ?");
+		stmt.setString(1, user.getPseudo());
+		stmt.setString(2, user.getPassword());
+		stmt.setString(3, user.getEmail());
+		stmt.executeUpdate();
+		stmt.close();
+		return false;
+	}
+
+	@Override
+	public User find(int id) throws SQLException {
+		stmt = connect.prepareStatement("SELECT * FROM User WHERE id_user = ?");
+		stmt.setInt(1, id);
+		ResultSet rs = stmt.executeQuery();
+		int id_user = rs.getInt("id_answer");
+		String pseudo = rs.getString("pseudo");
+		String password = rs.getString("password");
+		String email = rs.getString("email");
+		rs.close();
+		stmt.close();
+		return new User(id_user, pseudo, password, email);
+	}
+	
+	@Override
+	public ArrayList<User> getAll() throws SQLException {
+		ArrayList<User> listUsers = new ArrayList<User>();
+		stmt = connect.prepareStatement("SELECT COUNT(*) as count FROM User");
+		ResultSet rs = stmt.executeQuery();
+		int count = rs.getInt("count");
+		for(int i = 1; i<=count; i++){
+			listUsers.add(find(i));
+		}
+		rs.close();
+		stmt.close();
+		return listUsers;
+	}
+
+
+}
---
+++ src/POJO/User.java
@@ -0,0 +1,53 @@
+package POJO;
+
+public class User {
+
+	private int id;
+	private String pseudo;
+	private String password;
+	private String email;
+	
+	public User(int id, String pseudo, String password, String email) {
+		super();
+		this.id = id;
+		this.pseudo = pseudo;
+		this.password = password;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+}
